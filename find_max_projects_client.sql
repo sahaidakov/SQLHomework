6c3c05bebd450f4c0f931495f3c654b19528f048
Artem Sahaidakov <sahaidakov@gmail.com>
2024-09-06 08:36:37 +0100

    modify find_max_projects_client.sql

--- find_max_projects_client.sql
+++ find_max_projects_client.sql
@@ -1,1 +1,12 @@
-select client_id, count(*) as project_count from project group by client_id;
+WITH client_project_count AS (
+    SELECT t1.name, COUNT(t2.id) AS project_count
+    FROM project t2
+    JOIN client t1 ON t2.client_id = t1.id
+    GROUP BY t1.name
+)
+SELECT name, project_count
+FROM client_project_count
+WHERE project_count = (
+    SELECT MAX(project_count)
+    FROM client_project_count
+);
